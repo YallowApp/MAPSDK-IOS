d67f58bab651f17c0d70ffb08876448314c22a96
Mohammad Rbabah <mohammad.a.rababah@gmail.com>
2020-11-02 15:11:12 +0200

    first commit

---
+++ Example/Pods/Target Support Files/MAPSDK/MAPSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MAPSDK : NSObject
+@end
+@implementation PodsDummy_MAPSDK
+@end
---
+++ YalloMapFramework.framework/Headers/YalloMapFramework.h
@@ -0,0 +1,19 @@
+//
+//  YalloMapFramework.h
+//  YalloMapFramework
+//
+//  Created by Tikino Pawn on 11/2/20.
+//  Copyright © 2020 Yallow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YalloMapFramework.
+FOUNDATION_EXPORT double YalloMapFrameworkVersionNumber;
+
+//! Project version string for YalloMapFramework.
+FOUNDATION_EXPORT const unsigned char YalloMapFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YalloMapFramework/PublicHeader.h>
+
+
